50a927eed2c2fe28809d409c6951fe4a5ba7c027
Priscilla Harris <priscillaharris.m@outlook.com>
2020-04-17 14:59:57 -0600

    Added my project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = "hello";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(str[i]);
+
+            }
+        }
+    }
+}
