14ee428b43b494e6bb62bad902ee4ab6a5c5d379
Priscilla Harris <priscillaharris.m@outlook.com>
2020-04-23 15:04:59 -0600

    Added comments to the program

--- Program.cs
+++ Program.cs
@@ -4,11 +4,14 @@ namespace ReverseString
 {
     class MainClass
     {
+        //This program asks the user for a word that they want to reverse. It then reverses the string.
         public static void Main(string[] args)
         {
-            string str = "hello";
+            Console.WriteLine("Please enter a word or sentence that you would like to reverse: ");
 
-            for (int i = str.Length - 1; i >= 0; i--)
+            string str = Console.ReadLine();  //Converts the user input to string str
+
+            for (int i = str.Length - 1; i >= 0; i--)  //loops the str and prints off each letter in reverse
             {
                 Console.WriteLine(str[i]);
 
